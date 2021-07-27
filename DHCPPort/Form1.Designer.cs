56942f7b64a01ba607a2fa3fd854296e66031bd8
Rowen Simpson <rowensimpson1991@gmail.com>
2021-07-27 12:05:57 +0100

    Add files via upload

---
+++ DHCPPort/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace DHCPPort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.RefreshButton = new System.Windows.Forms.Button();
+            this.ClientDropBox = new System.Windows.Forms.ComboBox();
+            this.LogListBox = new System.Windows.Forms.ListBox();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.ClientListBox = new System.Windows.Forms.ListBox();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.Disconnect = new System.Windows.Forms.Button();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(146, 117);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(122, 33);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(274, 117);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(122, 33);
+            this.StopButton.TabIndex = 1;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // RefreshButton
+            // 
+            this.RefreshButton.Location = new System.Drawing.Point(958, 152);
+            this.RefreshButton.Name = "RefreshButton";
+            this.RefreshButton.Size = new System.Drawing.Size(122, 33);
+            this.RefreshButton.TabIndex = 2;
+            this.RefreshButton.Text = "Refresh";
+            this.RefreshButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.RefreshButton.UseVisualStyleBackColor = true;
+            this.RefreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
+            // 
+            // ClientDropBox
+            // 
+            this.ClientDropBox.FormattingEnabled = true;
+            this.ClientDropBox.Location = new System.Drawing.Point(958, 124);
+            this.ClientDropBox.Name = "ClientDropBox";
+            this.ClientDropBox.Size = new System.Drawing.Size(122, 21);
+            this.ClientDropBox.TabIndex = 4;
+            this.ClientDropBox.Text = "ClientDropBox";
+            this.ClientDropBox.SelectedIndexChanged += new System.EventHandler(this.ClientDropBox_SelectedIndexChanged);
+            // 
+            // LogListBox
+            // 
+            this.LogListBox.FormattingEnabled = true;
+            this.LogListBox.Location = new System.Drawing.Point(146, 156);
+            this.LogListBox.Name = "LogListBox";
+            this.LogListBox.Size = new System.Drawing.Size(400, 147);
+            this.LogListBox.TabIndex = 5;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(958, 230);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(122, 33);
+            this.ConnectButton.TabIndex = 6;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // ClientListBox
+            // 
+            this.ClientListBox.FormattingEnabled = true;
+            this.ClientListBox.Location = new System.Drawing.Point(552, 156);
+            this.ClientListBox.Name = "ClientListBox";
+            this.ClientListBox.Size = new System.Drawing.Size(400, 147);
+            this.ClientListBox.TabIndex = 7;
+            // 
+            // Disconnect
+            // 
+            this.Disconnect.Location = new System.Drawing.Point(958, 268);
+            this.Disconnect.Name = "Disconnect";
+            this.Disconnect.Size = new System.Drawing.Size(122, 33);
+            this.Disconnect.TabIndex = 8;
+            this.Disconnect.Text = "Disconnect";
+            this.Disconnect.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.Disconnect.UseVisualStyleBackColor = true;
+            this.Disconnect.Click += new System.EventHandler(this.Disconnect_Click);
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(958, 191);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(122, 33);
+            this.sendButton.TabIndex = 9;
+            this.sendButton.Text = "Send";
+            this.sendButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 27);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1266, 485);
+            this.tabControl1.TabIndex = 10;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.LogListBox);
+            this.tabPage1.Controls.Add(this.sendButton);
+            this.tabPage1.Controls.Add(this.StartButton);
+            this.tabPage1.Controls.Add(this.Disconnect);
+            this.tabPage1.Controls.Add(this.StopButton);
+            this.tabPage1.Controls.Add(this.ClientListBox);
+            this.tabPage1.Controls.Add(this.RefreshButton);
+            this.tabPage1.Controls.Add(this.ConnectButton);
+            this.tabPage1.Controls.Add(this.ClientDropBox);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1258, 459);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1258, 459);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1284, 515);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "1490 DHCP Server ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button RefreshButton;
+        private System.Windows.Forms.ComboBox ClientDropBox;
+        private System.Windows.Forms.ListBox LogListBox;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.ListBox ClientListBox;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.Button Disconnect;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ DHCPPort/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+
+using System.Windows.Forms;
+using DotNetProjects.DhcpServer;
+using TCPClientLib;
+
+
+namespace DHCPPort
+{
+    public partial class Form1 : Form
+    {
+        static byte nextIP = 10;
+        static Dictionary<string, IPAddress> leases = new Dictionary<string, IPAddress>();
+        DHCPServer server = null;
+        TCPClient tcpclient = null;
+        public Form1()
+        {
+            InitializeComponent();
+            Console.WriteLine("Initilized Completed");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("Form Loaded");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            server.Dispose();
+        }
+
+        static void Request(DHCPRequest dhcpRequest)
+        {
+            try
+            {
+                var type = dhcpRequest.GetMsgType();
+                var mac = ByteArrayToString(dhcpRequest.GetChaddr());
+                // IP for client
+                IPAddress ip;
+                if (!leases.TryGetValue(mac, out ip))
+                {
+                    ip = new IPAddress(new byte[] { 192, 168, 1, nextIP++ });
+                    leases[mac] = ip;
+                }
+                Console.WriteLine(type.ToString() + " request from " + mac + ", it will be " + ip.ToString());
+
+                var options = dhcpRequest.GetAllOptions();
+                Console.Write("Options:");
+                foreach (DHCPOption option in options.Keys)
+                {
+                    Console.WriteLine(option.ToString() + ": " + ByteArrayToString(options[option]));
+                }
+                // Lets show some request info
+                var requestedOptions = dhcpRequest.GetRequestedOptionsList();
+                if (requestedOptions != null)
+                {
+                    Console.Write("Requested options:");
+                    foreach (DHCPOption option in requestedOptions) Console.Write(" " + option.ToString());
+                    Console.WriteLine();
+                }
+                // Option 82 info
+                var relayInfoN = dhcpRequest.GetRelayInfo();
+                if (relayInfoN != null)
+                {
+                    var relayInfo = (RelayInfo)relayInfoN;
+                    if (relayInfo.AgentCircuitID != null)
+                        Console.WriteLine("Relay agent circuit ID: " + ByteArrayToString(relayInfo.AgentCircuitID));
+                    if (relayInfo.AgentRemoteID != null)
+                        Console.WriteLine("Relay agent remote ID: " + ByteArrayToString(relayInfo.AgentRemoteID));
+                }
+                Console.WriteLine();
+
+                var replyOptions = new DHCPReplyOptions();
+                // Options should be filled with valid data. Only requested options will be sent.
+                replyOptions.SubnetMask = IPAddress.Parse("255.255.255.0");
+                replyOptions.DomainName = "SharpDHCPServer";
+                replyOptions.ServerIdentifier = IPAddress.Parse("10.0.0.1");
+                replyOptions.RouterIP = IPAddress.Parse("10.0.0.1");
+                replyOptions.DomainNameServers = new IPAddress[]
+                {IPAddress.Parse("192.168.100.2"), IPAddress.Parse("192.168.100.3")};
+                // Some static routes
+                replyOptions.StaticRoutes = new NetworkRoute[]
+                {
+                    new NetworkRoute(IPAddress.Parse("10.0.0.0"), IPAddress.Parse("255.0.0.0"),
+                        IPAddress.Parse("10.0.0.1")),
+                    new NetworkRoute(IPAddress.Parse("192.168.0.0"), IPAddress.Parse("255.255.0.0"),
+                        IPAddress.Parse("10.0.0.1")),
+                    new NetworkRoute(IPAddress.Parse("172.16.0.0"), IPAddress.Parse("255.240.0.0"),
+                        IPAddress.Parse("10.0.0.1")),
+                    new NetworkRoute(IPAddress.Parse("80.252.130.248"), IPAddress.Parse("255.255.255.248"),
+                        IPAddress.Parse("10.0.0.1")),
+                    new NetworkRoute(IPAddress.Parse("80.252.128.88"), IPAddress.Parse("255.255.255.248"),
+                        IPAddress.Parse("10.0.0.1")),
+                };
+
+                // Lets send reply to client!
+                if (type == DHCPMsgType.DHCPDISCOVER)
+                    dhcpRequest.SendDHCPReply(DHCPMsgType.DHCPOFFER, ip, replyOptions);
+                if (type == DHCPMsgType.DHCPREQUEST)
+                    dhcpRequest.SendDHCPReply(DHCPMsgType.DHCPACK, ip, replyOptions);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        static string ByteArrayToString(byte[] ar)
+        {
+            var res = new StringBuilder();
+            foreach (var b in ar)
+            {
+                res.Append(b.ToString("X2"));
+            }
+            res.Append(" (");
+            foreach (var b in ar)
+            {
+                if ((b >= 32) && (b < 127))
+                    res.Append(Encoding.ASCII.GetString(new byte[] { b }));
+                else res.Append(" ");
+            }
+            res.Append(")");
+            return res.ToString();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            server = new DHCPServer();
+            server.ServerName = "SharpDHCPServer";
+            server.OnDataReceived += Request;
+            server.BroadcastAddress = IPAddress.Broadcast;
+            server.SendDhcpAnswerNetworkInterface = null;
+            server.Start();
+            Console.WriteLine("DHCPPort server Running");
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            server.Dispose();
+            Console.WriteLine("DHCPPort server stopped");
+        }
+
+        private void ClientDropBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            ClientDropBox.Items.Clear();
+            for (int i = 0; i < leases.Count; i++)
+            {
+                ClientDropBox.Items.Add(leases.ElementAt(i).Value);
+            }
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            var ipaddress = ClientDropBox.SelectedItem.ToString();
+            tcpclient = new TCPClient(IPAddress.Parse(ipaddress));
+        }
+
+        private void Disconnect_Click(object sender, EventArgs e)
+        {
+            if (tcpclient != null)
+            {
+                tcpclient.Disconnect();
+                Console.WriteLine("Socket Disconnected");
+            }
+            else
+            {
+                Console.WriteLine("Socket doesnt exist");
+            }
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            if (tcpclient != null)
+            {
+                //tcpclient.SendMessage();
+                tcpclient.SendMessage();
+                Console.WriteLine("Message Sent");
+            }
+            else
+            {
+                Console.WriteLine("Socket doesnt exist");
+            }
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
